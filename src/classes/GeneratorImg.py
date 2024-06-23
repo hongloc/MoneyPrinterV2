b30c6031dbfcc1d863adb3a98ff94847719b0936
Loc Hong <hongloc2206@gmail.com>
2024-06-23 12:34:54 +0700

    update

--- src/classes/GeneratorImg.py
+++ src/classes/GeneratorImg.py
@@ -2,7 +2,7 @@
 freeGPT's prodia module
 """
 
-from requests import get
+from requests import get, post
 from random import randint
 from requests.exceptions import RequestException
 import time
@@ -24,11 +24,24 @@ class Generation:
         """
         headers = {
             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36",
-            "X-Prodia-Key": "555889c5-0dd6-43af-b96c-a7d0aa8d6186"
+            # "X-Prodia-Key": "555889c5-0dd6-43af-b96c-a7d0aa8d6186"
         }
         try:
             changed_prompt = "Korean idol face. " + prompt
             print('changed_prompt: ', changed_prompt)
+            # payload = {
+            #     "model": "shoninsBeautiful_v10.safetensors [25d8c546]",
+            #     "prompt": changed_prompt,
+            #     "negative_prompt": "verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.5),cross-eyed,sketches, (worst quality:2), (low quality:2.1), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, repeating hair",
+            #     "width": 1024,
+            #     "height": 1024,
+            #     "sampler": "DPM++ 2M Karras",
+            #     "seed": randint(1, 10000),
+            #     "cfg_scale": 7,
+            #     "steps": 20
+            # }
+            # url = "https://api.prodia.com/v1/sd/generate"
+            # resp = post(url, json=payload, headers=headers)
             resp = get(
                 "https://api.prodia.com/generate",
                 params={
@@ -62,6 +75,7 @@ class Generation:
             data = resp.json()
             print('data: ', data)
             while True:
+                # resp = get(f"https://api.prodia.com/v1/job/{data['job']}", headers=headers)
                 resp = get(f"https://api.prodia.com/job/{data['job']}", headers=headers)
                 json = resp.json()
                 print('json: ', json)
